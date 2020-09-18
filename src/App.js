3b1d928265f30cd10de27d03eb03cdbfc3b27a74
CStank89 <C.Stank89@gmail.com>
2020-09-18 13:56:01 -0400

    working on the schema and yup

--- src/App.js
+++ src/App.js
@@ -21,7 +21,20 @@ const App = () => {
     setFormValues({...formValues, [name]: value})
   }
 
+  const submit = (evt) => {
+    //Prevents the default behavious of submit which is reloading the page
+    evt.preventDefault();
+  
 
+const newData = {
+    name: formValues.name.trim(),
+    size: formValues.size.trim(),
+    toppings: formValues.toppings.trim(),
+    speicalInstructions: formValues.speicalInstructions.trim(),
+    }
+    setSavedFormInfo([...savedFormInfo, newData])
+    setFormValues(defaultValues)
+};
 
   return (
     <div className='app'> 
--- src/Form.js
+++ src/Form.js
@@ -1,70 +1,138 @@
-import React, { useState } from 'react'
-import Pizza from './Pizza'
-import {Link} from 'react-router-dom'
+import React from 'react'
+
+// import {Link} from 'react-router-dom'
 
 
 const Form = (props) => {
-    const { formValues, change } = props
+    const { formValues, change, } = props
 
-
-
+  
     return (
         <div>
             
             <h1>Build Your Pizza</h1>
             <form>
+            
+            
                 <select>
-                    <option value={formValues}>Small</option>
-                    <option value={formValues}>Medium</option>
-                    <option value={formValues}>Large</option>
+                    <option value={formValues.size}>Small</option>
+                    <option value={formValues.size}>Medium</option>
+                    <option value={formValues.size}>Large</option>
                 </select>
                 <label>
                     <button>Submit</button>
                 </label>
-            <form>
+             
+            
+              
             <label htmlFor='sauce'><b>Choice of Sauce</b> </label> <br/>
             <label>Marinara Sause</label>
-            <input type='radio' name='sauce' /> <br/>
+            <input 
+            type='radio' 
+            name='sauce' 
+            onChange={change} > 
+            </input> <br/>
             <label>Garlic Ranch</label>
-            <input type='radio' name='sauce' /> <br/>
+            <input 
+            type='radio' 
+            name='sauce' 
+            onChange={change}>
+            </input> <br/>
             <label>BBQ Sauce</label>
-            <input type='radio' name='sauce' /> <br/>
+            <input 
+            type='radio' 
+            name='sauce' 
+            onChange={change}>
+            </input> <br/>
             <label>Spinage Alfredo</label>
-            <input type='radio' name='sauce' /> <br/>
-            </form>
-            <form>
+            <input 
+            type='radio' 
+            name='sauce' 
+            onChange={change}>
+                </input> <br/>
+           
+            
+           
             <label htmlFor='sauce'><b>Choice of Toppings</b> </label> <br/>
             <label>Pepperoni</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            onChange={change}
+            value={formValues.toppings}/> <br/>
             <label>Cheese</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings}
+            onChange={change} /> <br/>
             <label>Olives</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings} 
+            onChange={change}/> <br/>
             <label>Green Peppers</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings} 
+            onChange={change}/> <br/>
             <label>Onions</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings} 
+            onChange={change}/> <br/>
             <label>Ham</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings} 
+            onChange={change}/> <br/>
             <label>Pineapple</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings}
+            onChange={change}/> <br/>
             <label>Bacon</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings} 
+            onChange={change}/> <br/>
             <label>Roasted Garlic</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings} 
+            onChange={change}/> <br/>
             <label>Chicken</label>
-            <input type='checkBox' name='sauce' /> <br/>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings} 
+            onChange={change}/> <br/>
             <label>Sausage</label>
-            <input type='checkBox' name='sauce' /> <br/>
-            </form>
-            <form>
-                <label> <b>Special Instructions</b> </label> <br/>
-                <input 
-                type='text' 
-                placeholder='Anything else you would like to add'/>
-                
-            </form>
-            </form>
+            <input 
+            type='checkBox' 
+            name='sauce' 
+            value={formValues.toppings} 
+            onChange={change}/> <br/>
+            
+
+           
+            <label> <b>Special Instructions</b> </label> <br/>
+            <input 
+            type='text' 
+            placeholder='Anything else you would like to add'/> <br/>
+            <button>ADD TO ORDER</button>
+           
+    
+           
+
+     </form>
         
         </div>
     )
